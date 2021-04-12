7312d9f72a21855b050ba3602362ddc7df4051a4
Vinicius Sanchez <viniciuss.sanchez@gmail.com>
2021-04-12 20:07:16 -0300

    3.1 Hello world com o spring boot

---
+++ user-api/src/main/java/br/com/sanchez/userapi/controllers/UserController.java
@@ -0,0 +1,60 @@
+package br.com.sanchez.userapi.controllers;
+
+import br.com.sanchez.userapi.dto.UserDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    public static List<UserDTO> users = new ArrayList<UserDTO>();
+
+    @PostConstruct
+    public void init() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setNome("Vinicius");
+        userDTO.setCpf("43845970008");
+        userDTO.setEndereco("Rua A");
+        userDTO.setEmail("vinicius@gmail.com");
+        userDTO.setTelefone("17996448184");
+        userDTO.setDataCadastro(new Date());
+
+        UserDTO userDTO2 = new UserDTO();
+        userDTO2.setNome("Ingrid");
+        userDTO2.setCpf("65887448848");
+        userDTO2.setEndereco("Rua B");
+        userDTO2.setEmail("ingrid@gmail.com");
+        userDTO2.setTelefone("17996665058");
+        userDTO2.setDataCadastro(new Date());
+
+        UserDTO userDTO3 = new UserDTO();
+        userDTO3.setNome("Sandra");
+        userDTO3.setCpf("65889852214");
+        userDTO3.setEndereco("Rua C");
+        userDTO3.setEmail("sandra@gmail.com");
+        userDTO3.setTelefone("17998884652");
+        userDTO3.setDataCadastro(new Date());
+
+        users.add(userDTO);
+        users.add(userDTO2);
+        users.add(userDTO3);
+    }
+
+    @GetMapping("/")
+    public String getMensagem() {
+        return  "Spring bott is working!";
+    }
+
+    @GetMapping("/users")
+    public List<UserDTO> getUsers() {
+        return users;
+    }
+
+
+
+}
