8175a60dc71c31dea52c1e1ea6ffcad4f715d4af
Vinicius Sanchez <viniciuss.sanchez@gmail.com>
2021-04-13 19:46:06 -0300

    Criação dos novos serviços

---
+++ shopping-api/src/main/java/br/com/sanchez/shoppingapi/ShoppingApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sanchez.shoppingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingApiApplication.class, args);
+    }
+
+}
